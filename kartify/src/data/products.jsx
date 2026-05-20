f95243c399417a7136b1ccc308ca129e58aaec78
Sanmitraa <princess.sanmitra05@gmail.com>
2026-05-20 18:24:37 +0530

    first commit

---
+++ kartify/src/data/products.jsx
@@ -0,0 +1,18 @@
+const products = [
+  {
+    id: 1,
+    name: "Wireless Headphones",
+    price: 1999,
+    image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e",
+    category: "Electronics"
+  },
+  {
+    id: 2,
+    name: "Smart Watch",
+    price: 2999,
+    image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30",
+    category: "Electronics"
+  }
+];
+
+export default products;
---
+++ kartify/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+function Home() {
+  return (
+    <div className="px-6">
+      <h1 className="text-5xl font-bold mt-10">
+        Welcome to Kartify
+      </h1>
+
+      <p className="text-gray-400 mt-4 text-lg">
+        Discover premium products at the best prices.
+      </p>
+
+      <button className="mt-6 bg-gradient-to-r from-indigo-500 to-purple-600 px-6 py-3 rounded-xl hover:scale-105 transition duration-300">
+        Shop Now
+      </button>
+    </div>
+  )
+}
+
+export default Home
